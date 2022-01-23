7f6271ecff2d634a0cec1da5b0917c7439aa50a7
KirillNowoselow <98112009+KirillNowoselow@users.noreply.github.com>
2022-01-23 19:41:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 if __name__ == "__main__":
-    a = int(input("Введите число A (Ax^2+bx+c)"))
-    b = int(input("Введите число B (ax^2+Bx+c)"))
-    c = int(input("Введите число C (ax^2+bx+C)"))
-    D = b**2-4*a*c
+  a = int(input('Введите коеффициент а: '))
+  b = int(input('Введите коеффициент b: '))
+  c = int(input('Введите коеффициент c: '))
+  D = b**2-4*a*c
+  if D>0:
     x1 = (-b+D**(1/2))/(2*a)
     x2 = (-b-D**(1/2))/(2*a)
-    if D >= 0:
-        if D == 0:
-            print("X=", x1)
-        else:
-            print("X1=", x1, "X2=", x2)
-    else:
-        print("Нет корней")
+    print('X1= ', x1, 'X2= ', x2)
+  elif D==0:
+    x = (-b)/(2*a)
+    print('X= ', x)
+  else:
+    print('Нет корней')
